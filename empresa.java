1a72c525faca337c023480c6b4c4f8273460c8ff
brunomecca123 <brunomecca123@hotmail.com>
2022-09-15 20:25:14 -0300

    Adicionado arquivo Empresa

---
+++ empresa.java
@@ -0,0 +1,81 @@
+
+public class empresa {
+         private int cod_empresa;
+    //@Column(name="nome_Empresa")
+   private String nome_fantasia;
+    //@Column(name="cpf_Empresa")
+   private String cnpj_empresa;
+    //@Column(name="senha_empresa")
+   private String senha_empresa;
+    //@Column(name="endereco_empresa")
+   private String endereco_empresa;
+   private String ramo_empresa;
+   private  String porte_empresa;
+
+   
+   
+    public empresa(int cod_empresa, String nome_fantasia, String cnpj_Usuario, String endereco_empresa, String senha_empresa, String ramo_empresa, String porte_empresa) {
+        this.cod_empresa = cod_empresa;
+        this.nome_fantasia = nome_fantasia;
+        this.cnpj_empresa= cnpj_empresa;
+        this.senha_empresa = senha_empresa;
+        this.endereco_empresa = endereco_empresa;
+        this.ramo_empresa = ramo_empresa;
+        this.porte_empresa = porte_empresa;
+}
+
+    public int getCod_empresa() {
+        return cod_empresa;
+    }
+
+    public void setCod_empresa(int cod_empresa) {
+        this.cod_empresa = cod_empresa;
+    }
+
+    public String getNome_empresa() {
+        return nome_empresa;
+    }
+
+    public void setNome_empresa(String nome_empresa) {
+        this.nome_empresa = nome_empresa;
+    }
+
+    public String getCnpj_empresa() {
+        return cnpj_empresa;
+    }
+
+    public void setCnpj_empresa(String cnpj_empresa) {
+        this.cnpj_empresa = cnpj_empresa;
+    }
+
+    public String getSenha_empresa() {
+        return senha_empresa;
+    }
+
+    public void setSenha_empresa(String senha_empresa) {
+        this.senha_empresa = senha_empresa;
+    }
+
+    public String getEndereco_empresa() {
+        return endereco_empresa;
+    }
+
+    public void setEndereco_empresa(String endereco_empresa) {
+        this.endereco_empresa = endereco_empresa;
+    }
+
+    public String getRamo_empresa() {
+        return ramo_empresa;
+    }
+
+    public void setRamo_empresa(String ramo_empresa) {
+        this.ramo_empresa = ramo_empresa;
+    }
+
+    public String getPorte_empresa() {
+        return porte_empresa;
+    }
+
+    public void setPorte_empresa(String porte_empresa) {
+        this.porte_empresa = porte_empresa;
+    }
