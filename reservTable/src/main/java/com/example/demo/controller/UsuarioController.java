11b98259d7ec95eba2f193a156fe225281cd4da7
vaquejano <vaquejano@gmail.com>
2022-09-27 19:08:42 -0300

    Usuario pronto

--- reservTable/src/main/java/com/example/demo/controller/UsuarioController.java
+++ reservTable/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -3,10 +3,16 @@ package com.example.demo.controller;
 import com.example.demo.model.Usuario;
 import com.example.demo.service.UsuarioService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +27,27 @@ public class UsuarioController {
     public ResponseEntity <List<Usuario>> listaUsuarios(){
         return ResponseEntity.status(HttpStatus.OK). body(usuarioService.listaUsuarios());
     }
+   
+    @GetMapping("usuario/{codusuario}")
+    public ResponseEntity<Optional<Usuario>> getByIdUsuario(@PathVariable Integer codusuario){
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioService.getByIdUsuario(codusuario));
+    }
+    
+    @PostMapping("usuario")
+    public ResponseEntity<Usuario> salvaUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.salvaUsuario(usuario));
+    }
+    
+    @PutMapping("usuario")
+    public ResponseEntity<Usuario> atualizaUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioService.atualizaUsuario(usuario));
+    }
+    
+    @DeleteMapping("usuario/{codusuario}")
+    public ResponseEntity<String> deleteByIdUsuario(@PathVariable Integer codusuario){
+        usuarioService.deleteByIdUsuario(codusuario);
+        return ResponseEntity.status(HttpStatus.OK).body("Usuario removido com sucesso");
+    } 
     
     
     
--- reservTable/src/main/java/com/example/demo/service/UsuarioServiceImpl.java
+++ reservTable/src/main/java/com/example/demo/service/UsuarioServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.Usuario;
 import com.example.demo.repository.UsuarioRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +16,27 @@ public class UsuarioServiceImpl implements UsuarioService {
     public List<Usuario> listaUsuarios() {
         return usuarioRepository.findAll();
     }
+    @Override
+    public Optional<Usuario> getByIdUsuario(Integer codUsuario) {
+        return usuarioRepository.findById(codUsuario);
+    }
+    
+    @Override
+    public Usuario salvaUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
     
 
+    @Override
+    public Usuario atualizaUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 
+    @Override
+    public void deleteByIdUsuario(Integer codUsuario) {
+       usuarioRepository.deleteById(codUsuario);
+    }
+ 
 
 }
